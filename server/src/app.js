b037ccc0e4b7e022577c0dc898ab42b0f4f39b47
Dionel22 <diondiaz293@gmail.com>
2023-11-01 13:05:51 -0300

    configuracion de app

---
+++ server/src/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParse = require("cookie-parser");
+const morgan = require("morgan");  
+const cors = require("cors");
