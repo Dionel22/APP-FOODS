94a971466a12d2863954d925ef012b1b6a2ff5fa
Dionel22 <diondiaz293@gmail.com>
2023-11-02 20:45:14 -0300

    configuracion de handlers y controllers

--- server/src/controllers/ControllerFilted/controllerFilted.js
+++ server/src/controllers/ControllerFilted/controllerFilted.js
@@ -1,19 +1,49 @@
+require('dotenv').config();
+const { DB_APi_KEY } = process.env;
 const { Op } = require('sequelize');
 const { recipe, diet } = require("../../db");
 
 const filted = async (filters) => {
   if (filters.api === "API") {
+    console.log("api", filters);
     // Hacer la llamada a la API y transformar los datos, como lo hiciste antes
-    const apiData = await fetch(`https://api.spoonacular.com/recipes/complexSearch?number=100&offset=0&addRecipeInformation=true&apiKey=${DB_API_KEY}`)
+    const apiData = await fetch(`https://api.spoonacular.com/recipes/complexSearch?number=100&offset=0&addRecipeInformation=true&apiKey=${DB_APi_KEY}`)
       .then((data) => data.json())
       .then((data) => {
         return data.results.map((e) => {
           // Transformar los datos de la API
           return {
-            // ...
+            id: e.id,
+            title: e.title,
+            image: e.image,
+            summary: e.summary.replace(/<[^>]*>/g, ""),
+            healthScore: e.healthScore,
+            steps: e.analyzedInstructions[0]?.steps.map((el)=>{
+                return{
+                    number: el.number,
+                    step: el.step
+                }
+            }),
+            diets: e.diets.map(e=>{
+                return{name: e}
+            }),
           };
         });
       });
+
+      let response;
+      if(filters.title != null){
+        console.log("entra 1");
+         response = await apiData.filter((e)=> e.title.includes(filters.title));
+      }
+      if(filters.diet != null){
+        console.log("entra 2");
+         response = await apiData.filter((e)=> e.diets?.some(diet => diet.name === filters.diet));
+      }
+      console.log("res", response);
+      if(filters.title != null || filters.diet != null){
+        return response;
+      }
     
     return apiData;
   }
