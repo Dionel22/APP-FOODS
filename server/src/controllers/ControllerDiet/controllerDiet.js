c3f738e39ef4ff29ff8cbd4fab22311f58c25b55
Dionel22 <diondiaz293@gmail.com>
2023-11-02 19:59:23 -0300

    configuracion de handlers y controllers

--- server/src/controllers/ControllerDiet/controllerDiet.js
+++ server/src/controllers/ControllerDiet/controllerDiet.js
@@ -1,34 +1,34 @@
 const { diet } = require("../../db");
 require('dotenv').config();
 const { DB_APi_KEY } = process.env;
-  
+
 const getAllDietsDB = async () => {
-    const responseDb = await diet.findAll({attributes: ["name"]});
+  // Intenta obtener las dietas de la base de datos local
+  const responseDb = await diet.findAll({ attributes: ["name"] });
 
-   if(responseDb.length === 0){
-   const responseApi = await fetch(`https://api.spoonacular.com/recipes/complexSearch?number=100&offset=0&addRecipeInformation=true&apiKey=${DB_APi_KEY}`)
-   .then(data => data.json())
-   .then((responseApi) =>{
-    const responseDiets = responseApi.results.map((e) =>{ 
-    return  e.diets?.map(diet => {
-        return {name: diet}
-      }) 
-   })
-   return responseDiets;
-   })
-   const transforma = responseApi.flatMap(e => e);
-   console.log("api", transforma );
+  // Si no se encuentran dietas en la base de datos, realiza una llamada a la API
+  if (responseDb.length === 0) {
+    // Realiza una llamada a la API para obtener datos de recetas
+    const responseApi = await fetch(`https://api.spoonacular.com/recipes/complexSearch?number=100&offset=0&addRecipeInformation=true&apiKey=${DB_APi_KEY}`)
+      .then(data => data.json())
+      .then(responseApi => {
+        const responseDiets = responseApi.results.flatMap(e => e.diets?.map(diet => ({ name: diet })));
+        return responseDiets;
+      });
 
-   transforma.forEach(e=>{
-        diet.findOrCreate({where:{name: `${e.name}`}});
+    // Almacena las dietas en la base de datos
+    responseApi.forEach(e => {
+      diet.findOrCreate({ where: { name: e.name } });
     });
-    
-    const response = await diet.findAll({attributes: ["name"]});
-    return response;
-}
- 
- 
-  return responseDb;
-}
 
-  module.exports =  getAllDietsDB;
+    // Realiza otra consulta para obtener las dietas actualizadas
+    const response = await diet.findAll({ attributes: ["name"] });
+    return response;
+  }
+
+  // Si se encontraron dietas en la base de datos, simplemente devuelve esas dietas
+  return responseDb;
+};
+
+
+module.exports =  getAllDietsDB;
--- server/src/controllers/ControllerFilted/controllerFilted.js
+++ server/src/controllers/ControllerFilted/controllerFilted.js
@@ -1,85 +1,50 @@
+const { Op } = require('sequelize');
 const { recipe, diet } = require("../../db");
 
-const filted = async (filtro) => {
-    if(filtro.api === "API"){
-        const api = await fetch(`https://api.spoonacular.com/recipes/complexSearch?number=100&offset=0&addRecipeInformation=true&apiKey=${DB_APi_KEY}`)
-    .then((data)=> data.json())
-    .then(data => {
-        return data.results.map(e =>{
-            return {
-                id: e.id,
-                title: e.title,
-                image: e.image,
-                summary: e.summary.replace(/<[^>]*>/g, ""),
-                healthScore: e.healthScore,
-                steps: e.analyzedInstructions[0]?.steps.map((el)=>{
-                    return{
-                        number: el.number,
-                        step: el.step
-                    }
-                }),
-                diets: e.diets.map(e=>{
-                    return{name: e}
-                }),
-            }
-        })
-    })
-    return api;
-}
-
-    let whereClause = {};
-
-    if (filters.title) {
-        const searchQuery = `%${filters.title}%`; // No es necesario convertir a minúsculas en este caso
-        whereClause.name = {
-          [Op.iLike]: searchQuery,
-        };
-      }
-    
-      // Buscar libros por precio [1000, 5000]
-      //console.log("filter",typeof filters.price)
-      if (filters.price) {
-        const priceArray = filters.price.split(',').map(Number);
-        if (priceArray.length === 2) {
-         // console.log("<--<>", priceArray);
-          whereClause.price = {
-            [Op.between]: priceArray,
+const filted = async (filters) => {
+  if (filters.api === "API") {
+    // Hacer la llamada a la API y transformar los datos, como lo hiciste antes
+    const apiData = await fetch(`https://api.spoonacular.com/recipes/complexSearch?number=100&offset=0&addRecipeInformation=true&apiKey=${DB_API_KEY}`)
+      .then((data) => data.json())
+      .then((data) => {
+        return data.results.map((e) => {
+          // Transformar los datos de la API
+          return {
+            // ...
           };
-        }
-      }
+        });
+      });
     
-      // Buscar libros con fecha de Lanzamiento
-      //console.log("fecha",filters.releaseDate)
-      if (filters.releaseDate) {
-        const [startDate, endDate] = filters.releaseDate.split(',');
-      
-        whereClause.releaseDate = {
-          [Op.between]: [startDate.trim(), endDate.trim()],
-        };
-      }
-    
-      // Si se proporciona el nombre del autor, buscamos el ID del autor para la consulta
-      if (filters.author) {
-        const author = await Author.findOne({ where: { name: filters.author } });
-        if (author) {
-            whereClause.AuthorId = author.dataValues.id;
-            //console.log("--<",whereClause)
-        }
-      }
-    
-      // Si se proporciona el género del libro, buscamos el ID del género para la consulta
-      if (filters.diet) {
-        const diet = await diet.findOne({ where: { name: filters.diet } });
-        if (diet) {
-            //console.log(gender.dataValues)
-          whereClause.dietId = diet.dataValues.id;
-        }
-      }
-    
-    
-      const result = await recipe.findAll({ where: whereClause, include: [diet] });
-      //console.log("<--->",result)
-       return result;
-}
+    return apiData;
+  }
+
+  // Si no estás usando la API, construir la cláusula de búsqueda en la base de datos
+  const whereClause = {};
+
+  if (filters.title) {
+    const searchQuery = `%${filters.title}%`;
+    whereClause.title = {
+      [Op.iLike]: searchQuery,
+    };
+  }
+
+  if (filters.diet) {
+    // Si se proporciona un filtro de dieta, buscamos las recetas que cumplen con esa dieta
+    whereClause['$diets.name$'] = filters.diet;
+  }
+
+  const result = await recipe.findAll({
+    where: whereClause,
+    attributes: ["id", "title", "image", "summary", "healthScore", "steps"],
+    include: {
+      model: diet,
+      attributes: ["name"],
+      through: { attributes: [] },
+    },
+  });
+
+  return result;
+};
+
 
 module.exports = filted;
